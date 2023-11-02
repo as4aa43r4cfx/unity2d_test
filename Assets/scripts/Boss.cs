34188cb7f06a8ecedc9920b76c6667e17ec5b27f
as4aa43r4cfx <148439620+as4aa43r4cfx@users.noreply.github.com>
2023-11-02 18:49:47 +0900

    Update Boss.cs

--- Assets/scripts/Boss.cs
+++ Assets/scripts/Boss.cs
@@ -7,9 +7,15 @@ public class Boss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        ScoreManager js  = GameObject.Find("Scoremanager").GetComponent<Scoremanager>();
 
-        
+        if (js.currentScore > 25)
+        {
+
+        }
+
+
+
     }
 
     // Update is called once per frame
