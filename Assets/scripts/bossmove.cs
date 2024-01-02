7d5fb7566cc3e56285004bbaecd0da436b82e12f
as4aa43r4cfx <148439620+as4aa43r4cfx@users.noreply.github.com>
2024-01-02 15:50:59 +0900

    d
    d

--- Assets/scripts/bossmove.cs
+++ Assets/scripts/bossmove.cs
@@ -4,36 +4,62 @@ using UnityEngine;
 
 public class bossmove : MonoBehaviour
 {
+    Vector3 dir;
+    GameObject player;
+    public float speed = 5;
+    Vector3 pos;
 
 
-    public Transform target;
-    public Vector3 direction;
-    public float velocity;
-    public float default_velocity;
-    public float accelaration;
-    public Vector3 default_direction;
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
+        if (player != null)
+        {
+            int random = Random.Range(0, 100);
+
+            if (random < 50)
+            {
+                dir = Vector3.left;
+
+            }
+            else
+            {
+                dir = Vector3.right;
+            }
 
 
 
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ScoreManager cs = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
-        if (cs.currentScore > 2)
+        
+
+        pos = this.GameObject.transform.position;
+        
+        
+        if (pos.x > 2.5) 
         {
-            default_direction.x = Random.Range(-1.0f, 1.0f);
-            default_direction.y = Random.Range(-1.0f, 1.0f);
+            dir = Vector3.left;
+               
         }
 
+        if (pos.x < -2.5)
+        {
+            dir = Vector3.right;
 
+        }
 
+        transform.position += dir * speed * Time.deltaTime;
     }
     
 
